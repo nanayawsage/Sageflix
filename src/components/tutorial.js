9cba5eb39e4eaf65a14e9d2698b155fbbbdfae3e
Joseph <joseph.abuanor@amalitech.com>
2023-10-10 16:36:46 +0000

    aligning items

---
+++ src/components/tutorial.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "../assets/css/tutorial.scss"
+export default function tutorial() {
+  return (
+    <>
+    <div className="mybar">
+      <div>
+        <div className="myShapes myCircle"></div>
+        <div className="myShapes mySquare"></div>
+      </div>
+      <div className="myShapes myProfileCircle"></div>
+    </div>
+  </>
+  )
+}
